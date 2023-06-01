7d2866dc1aa6611bb6668ae2c25e24f3893ce3ea
MaryumShouket(Contra <MaryumShouket(Contra@86S4JR3>
2023-06-01 12:35:52 +0100

    Add project files.

---
+++ Controllers/BMIController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BMI_calculator_with_Jquery.Controllers
+{
+    public class BMIController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Calculate(double? weight, double? height)
+        {
+            if (weight.HasValue && height.HasValue)
+            {
+                if (height.Value != 0)
+                {
+                    var bmi = weight.Value / (height.Value * height.Value);
+                    return View("Index", bmi);
+                }
+                else
+                {
+                    ViewData["Error"] = "Height cannot be zero.";
+                    return View("Index");
+                }
+            }
+            else
+            {
+                ViewData["Error"] = $"Invalid weight or height.";
+                return View("Index");
+            }
+        }
+
+
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BMI_calculator_with_Jquery.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
